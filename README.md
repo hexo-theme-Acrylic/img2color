8d3ac8daa68455094d2f85ec1eb8bab534d49621
Marcus <marcus-zh@qq.com>
2023-07-16 20:11:44 +0800

    add:README,fix:Bug

---
+++ README.md
@@ -0,0 +1,5 @@
+## img2color API
+这是一个利用PIL的getpixel()来获取图片主题色
+适用于[Acrylic-Pro主题](https://github.com/hexo-theme-Acrylic/hexo-theme-Acrylic)的提取主题色
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fhexo-theme-Acrylic%2Fimg2color&project-name=hexo-theme-acrylic-api&repository-name=hexo-theme-acrylic-api&redirect-url=https%3A%2F%2Fgithub.com%2Fhexo-theme-Acrylic%2Fimg2color)
+~想法看上去挺蠢的,不会真有人用api吧,时不时504TimeOut~
--- app.py
+++ app.py
@@ -1,38 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask import Flask, request
-from api.img2color import img_to_color
-
+from PIL import Image
+import requests
+import io
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return "This is Marcus's API, /v1/ is public, /v2/ is private."
-
-@app.route('/v1/')
-def index_v1():
-  return "This is Marcus's public API."
-
-@app.route('/v2/')
-def index_v2():
-  return "This is Marcus's private API. "
+  return "This is img2color API."
 
 # theme_color
-@app.route('/v1/img2color', methods=['POST', 'GET'])
+@app.route('/img2color', methods=['POST', 'GET'])
 def img2color():
   if request.method == 'GET':
-      image_url = request.args.get('img')  # 从查询参数中获取图片链接
+    image_url = request.args.get('img')  # 从查询参数中获取图片链接
   elif request.method == 'POST':
-      image_url = request.form.get('img')  # 从请求体中获取图片链接
-
+    image_url = request.form.get('img')  # 从请求体中获取图片链接
   if not image_url:
-      return "No image URL provided. Please pass the '?img=' parameter when invoking the img", 400
+    return "No image URL provided. Please pass the '?img=' parameter when invoking the img", 400
 
-  main_color = img_to_color(image_url)  # 调用颜色提取函数
-
-  return main_color
-
+  response = requests.get(image_url)
+  image = Image.open(io.BytesIO(response.content))
+  image.thumbnail((100, 100))
+  main_color = image.getpixel((0, 0))
+  hex_color = '#%02x%02x%02x' % main_color
+  return hex_color
 
 if __name__ == '__main__':
     app.run()
