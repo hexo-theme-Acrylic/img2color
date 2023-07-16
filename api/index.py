21650107ff521a4e3ccd558a4133cdbba41aa982
Fan-Wu <121149519+timigomen@users.noreply.github.com>
2023-07-16 20:03:15 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ import io
 
 def img_to_color(image_url):
   response = requests.get(image_url)
-  image = Image.已打开(io.BytesIO(response.content))
+  image = Image.open(io.BytesIO(response.content))
 
   # 调整图像大小，加快颜色提取速度
   image.thumbnail((100, 100))
@@ -13,6 +13,6 @@ def img_to_color(image_url):
   main_color = image.getpixel((0, 0))
 
   # 将颜色转换为十六进制表示形式
-  hex_color = '#%02x%02x%02x' % main_color
+  hex_color = '#{:02x}{:02x}{:02x}'.format(*main_color)
 
   return hex_color
