21d3c50bd737971adf0ee9e66282e66a43f1e706
Fan-Wu <121149519+timigomen@users.noreply.github.com>
2023-07-26 19:09:04 +0800

    将输出格式优化,直接对接python版

--- img2color.php
+++ img2color.php
@@ -16,7 +16,7 @@ function getImageThemeColor($imageUrl) {
     $b = $rgb & 0xFF;
     
     // 转换为指定格式
-    $color = sprintf("0x%02x%02x%02x", $r, $g, $b);
+    $color = sprintf("#%02x%02x%02x", $r, $g, $b);
     
     // 返回RGB值
     return $color;
@@ -26,37 +26,13 @@ function getImageThemeColor($imageUrl) {
 if (isset($_GET['img'])) {
     $imageUrl = $_GET['img'];
     
-    // 检查是否已经存在对应链接的色彩信息
-    $jsonFile = 'colors.json';
-    $colors = array();
+    // 获取图片主题色
+    $themeColor = getImageThemeColor($imageUrl);
     
-    if (file_exists($jsonFile)) {
-        $json = file_get_contents($jsonFile);
-        $colors = json_decode($json, true);
-    }
-    
-    // 检查是否已经存储了对应链接的色彩信息
-    if (isset($colors[$imageUrl])) {
-        // 直接读取已存储的色彩信息并输出
-        $themeColor = $colors[$imageUrl];
-        $result = json_encode(array('RGB' => $themeColor));
-        echo $result;
-    } else {
-        // 获取图片主题色
-        $themeColor = getImageThemeColor($imageUrl);
-        
-        // 将色彩信息存储到数组中
-        $colors[$imageUrl] = $themeColor;
-        
-        // 将数组转换为JSON格式并存储到文件中
-        $json = json_encode($colors);
-        file_put_contents($jsonFile, $json);
-        
-        // 输出JSON格式的结果
-        $result = json_encode(array('RGB' => $themeColor));
-        echo $result;
-    }
+    // 输出主题色值
+    echo $themeColor;
 } else {
     // 如果未传入img参数，则返回错误信息
     echo '错误，请在URL中加上?img=';
 }
+?>
